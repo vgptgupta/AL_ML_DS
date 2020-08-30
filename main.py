31294a336d6d055b8d6c1d8670cd5e8587dccf4b
Vishal Gupta <vgpt.gupta@gmail.com>
2020-08-30 12:49:56 +0530

    Changed main,py for checking Anaconda env is working as expected or not.

--- main.py
+++ main.py
@@ -6,11 +6,26 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi, {0}'.format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import sys
+from termcolor import colored, cprint
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'magenta', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
